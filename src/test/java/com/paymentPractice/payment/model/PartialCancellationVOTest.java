a06603e1d8cf7f9f7e109ff5063b265977c0c1a9
hangsuu <ohhangsu@naver.com>
2024-05-27 21:41:00 +0900

    PartialCancellationVO 테스트

--- src/test/java/com/paymentPractice/payment/model/PartialCancellationVOTest.java
+++ src/test/java/com/paymentPractice/payment/model/PartialCancellationVOTest.java
@@ -1,6 +1,7 @@
 package com.paymentPractice.payment.model;
 
 import com.paymentPractice.common.exception.CustomException;
+import com.paymentPractice.payment.entity.YesOrNo;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -59,5 +60,59 @@ public class PartialCancellationVOTest {
         Assertions.assertThatThrownBy(() -> {
             PartialCancellationVO partialCancellationVO = new PartialCancellationVO(partialCancellationSO);
         }).isInstanceOf(CustomException.class);
+
+        partialCancellationSO.setAmount(1000000001);
+        Assertions.assertThatThrownBy(() -> {
+            PartialCancellationVO partialCancellationVO = new PartialCancellationVO(partialCancellationSO);
+        }).isInstanceOf(CustomException.class);
+
+        partialCancellationSO.setAmount(10000);
+        PartialCancellationVO partialCancellationVO = new PartialCancellationVO(partialCancellationSO);
+    }
+
+    @Test
+    void partialCancellationVOConstructorVatValidationCheckTest(){
+        PartialCancellationSO partialCancellationSO = new PartialCancellationSO();
+        partialCancellationSO.setAmountId("testAmountID");
+        partialCancellationSO.setVat(10001);
+        partialCancellationSO.setAmount(10000);
+
+        Assertions.assertThatThrownBy(() -> {
+            PartialCancellationVO partialCancellationVO = new PartialCancellationVO(partialCancellationSO);
+        }).isInstanceOf(CustomException.class);
+
+        partialCancellationSO.setVat(-1);
+
+        Assertions.assertThatThrownBy(() -> {
+            PartialCancellationVO partialCancellationVO = new PartialCancellationVO(partialCancellationSO);
+        }).isInstanceOf(CustomException.class);
+
+        partialCancellationSO.setVat(50);
+        PartialCancellationVO partialCancellationVO1 = new PartialCancellationVO(partialCancellationSO);
+
+        partialCancellationSO.setVat(null);
+        PartialCancellationVO partialCancellationVO2 = new PartialCancellationVO(partialCancellationSO);
+    }
+
+    @Test
+    void partialCancellationVOConstructorVatCalculateTest() {
+        PartialCancellationSO partialCancellationSO = new PartialCancellationSO();
+        partialCancellationSO.setAmountId("testAmountID");
+        partialCancellationSO.setVat(1000);
+        partialCancellationSO.setAmount(10000);
+
+        PartialCancellationVO partialCancellationVO1 = new PartialCancellationVO(partialCancellationSO);
+        Assertions.assertThat(partialCancellationVO1.getCalculatedVat()).isEqualTo(1000);
+        Assertions.assertThat(partialCancellationVO1.getVatDefaultYn()).isEqualTo(YesOrNo.N);
+
+        partialCancellationSO.setVat(null);
+        PartialCancellationVO partialCancellationVO2 = new PartialCancellationVO(partialCancellationSO);
+        Assertions.assertThat(partialCancellationVO2.getCalculatedVat()).isEqualTo((int) Math.round((double) 10000 / 11.0));
+        Assertions.assertThat(partialCancellationVO2.getVatDefaultYn()).isEqualTo(YesOrNo.Y);
+
+        partialCancellationSO.setVat((int) Math.round((double) 10000 / 11.0));
+        PartialCancellationVO partialCancellationVO3 = new PartialCancellationVO(partialCancellationSO);
+        Assertions.assertThat(partialCancellationVO3.getCalculatedVat()).isEqualTo((int) Math.round((double) 10000 / 11.0));
+        Assertions.assertThat(partialCancellationVO3.getVatDefaultYn()).isEqualTo(YesOrNo.N);
     }
 }
